efa1917571ced27c5b93b668e85452403888acb4
mar-cial <cesarmarcialq@gmail.com>
2022-03-03 22:09:35 -0800

    changed name from experiments to Project Bytes. Updated some files relative to the name change and added favicon

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Experiments
+# Project Bytes
 
 I will be adding small components every day to practice react.
 Started on march 1 2022.
--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,17 @@
-import Head from 'next/head'
-import MainLayout from '../layout/mainlayout'
-import '../styles/globals.css'
+import Head from 'next/head';
+import MainLayout from '../layout/mainlayout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <MainLayout>
       <Head>
-        <title>experiments</title>
+        <title>Project Bytes</title>
+        <link rel="icon" type="image/x-icon" href="/logo.ico" />
       </Head>
       <Component {...pageProps} />
     </MainLayout>
-
-
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
