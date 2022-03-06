964df6cab974ae62b5a241b4a450e9154eb3fc3d
mar-cial <cesarmarcialq@gmail.com>
2022-03-05 18:51:19 -0800

    day 5. SPA - Flex Strip

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,32 @@ import PageTitle from '../components/pageTitle';
 import CounterSection from '../sections/counterSection';
 import SharedLayoutSection from '../sections/sharedLayoutSection';
 import NetworkingSection from '../sections/networkingSection';
-import { motion } from 'framer-motion';
-import { FC } from 'react';
+import {motion} from 'framer-motion';
+import {FC} from 'react';
 import CartSection from '../sections/cartSection';
+import SpaSection from "../sections/spaSection";
 
 const Home: FC = () => {
-  return (
-    <motion.main className="grid gap-6" layout>
-      <PageTitle title="Home" />
+    return (
+        <motion.main className="grid gap-6" layout>
+            <PageTitle title="Home"/>
 
-      {/* Counter section */}
-      <CounterSection />
+            {/* Counter section */}
+            <CounterSection/>
 
-      {/* Shared layout section */}
-      <SharedLayoutSection />
+            {/* Shared layout section */}
+            <SharedLayoutSection/>
 
-      {/* Networking section */}
-      <NetworkingSection />
+            {/* Networking section */}
+            <NetworkingSection/>
 
-      {/* Typescript cart section */}
-      <CartSection />
-    </motion.main>
-  );
+            {/* Typescript cart section */}
+            <CartSection/>
+
+            {/* SPA */}
+            <SpaSection />
+        </motion.main>
+    );
 };
 
 export default Home;
---
+++ sections/spaSection.tsx
@@ -0,0 +1,32 @@
+/*
+* Created by cesarmarcial on 05/03/22
+*/
+
+// imports
+
+// interface
+
+// Beginning of component: spaSection
+import SectionHeader from "../components/sectionHeader";
+import ArticleReasonHeader from "../components/articleReasonHeader";
+import React, {FC} from "react";
+
+const SpaSection: FC = () => {
+    return (
+        <section>
+            <article>
+                <SectionHeader text={'SPA - FLEX STRIP'} date={'March 5, 2022'}/>
+                <ArticleReasonHeader text="Why I did this"/>
+                <p>The main objective of this project is to create a template for small businesses with a small catalog of products. I used the concept of a store called "FLEX STRIP" that sells PVC plastic rolls, with 3 basic pages: Home, Products and Contact.</p>
+                <p>I liked the page transitions implemented, it makes a very small application feel fluid and interactive.</p>
+                <p>Some tailwindcss doesn't work (as usual), I will fix it tomorrow.</p>
+                <p>Here's the <a href={'https://flexstrip.vercel.app'} className="text-orange-500">link</a> to the
+                    website, and here's the <a href={'https://flexstrip.vercel.app'} className="text-orange-500">repo</a> </p>
+
+
+            </article>
+        </section>
+    )
+}
+
+export default SpaSection
