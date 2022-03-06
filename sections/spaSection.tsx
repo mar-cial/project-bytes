ed67178286f9a44d851be2ab12f9f06b0b4ead30
mar-cial <cesarmarcialq@gmail.com>
2022-03-05 18:54:06 -0800

    typo

--- sections/spaSection.tsx
+++ sections/spaSection.tsx
@@ -21,7 +21,7 @@ const SpaSection: FC = () => {
                 <p>I liked the page transitions implemented, it makes a very small application feel fluid and interactive.</p>
                 <p>Some tailwindcss doesn't work (as usual), I will fix it tomorrow.</p>
                 <p>Here's the <a href={'https://flexstrip.vercel.app'} className="text-orange-500">link</a> to the
-                    website, and here's the <a href={'https://flexstrip.vercel.app'} className="text-orange-500">repo</a> </p>
+                    website, and here's the <a href={'https://github.com/mar-cial/flexstrip'} className="text-orange-500">repo.</a> </p>
 
 
             </article>
